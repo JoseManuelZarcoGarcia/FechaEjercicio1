d8dec5775488fee6db94db6bb24b8dad3455a3a6
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2024-01-29 14:28:41 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FechaEjercicio1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+            DateTime fecha4 = DateTime.Now;
+            fecha4.ToString("yy - MM - ddThh:mm: ss.ms");
+            Console.WriteLine("Formato fecha - tiempo: " + fecha4.ToString());
+
+
+        }
+
+    }
+
+}
